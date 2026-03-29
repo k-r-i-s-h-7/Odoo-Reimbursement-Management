34e946657e1211b6dd05f1d946a09025905cad99
dhruvsshah2005 <dhruvsshah2005@gmail.com>
2026-03-29 11:20:26 +0530

    feat: saved manager dashboard UI progress

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import ManagerDashboard from './pages/ManagerDashboard'; // We'll create this folder/file next
 
 const App = () => {
   return (
-    <div>App</div>
-  )
+    <Router>
+      <nav style={{ padding: '10px', background: '#1e1e1e', borderBottom: '1px solid #333' }}>
+        {/* Hackathon Navigation - Helps you jump to your part easily */}
+        <Link to="/manager" style={{ color: '#4da6ff', textDecoration: 'none' }}>Manager Dashboard</Link>
+      </nav>
+
+      <Routes>
+  {/* Making your work the default home page */}
+  <Route path="/" element={<ManagerDashboard />} /> 
+  <Route path="/manager" element={<ManagerDashboard />} />
+  </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
